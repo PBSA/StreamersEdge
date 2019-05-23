816e20581296c774a605589900a697a8f0f79f63
pixelplex <dev@pixelplex.by>
2019-05-23 21:12:36 +0300

    chore: fix eslint errors after merge

--- test/mock/connections/facebook.connection.mock.js
+++ test/mock/connections/facebook.connection.mock.js
@@ -2,30 +2,29 @@ const constants = require('../../constants.json');
 
 class FacebookConnectionMock {
 
-	connect() {}
+  connect() {}
 
-	async userInfo(code) {
-		if (code === constants.modules.api.auth.facebookValidCode) {
-			return {
-				name: constants.modules.api.auth.facebookTestUsername,
-				email: constants.modules.api.auth.facebookTestEmail,
-				picture:
-					{
-						data:
-							{
-								height: 50,
-								is_silhouette: true,
-								url: constants.modules.api.auth.facebookTestPicture,
-								width: 50,
-							},
-					},
-				id: constants.modules.api.auth.facebookTestId,
-			};
-		}
-		throw new Error('Invalid code');
-	}
+  async userInfo(code) {
+    if (code === constants.modules.api.auth.facebookValidCode) {
+      return {
+        name: constants.modules.api.auth.facebookTestUsername,
+        email: constants.modules.api.auth.facebookTestEmail,
+        picture: {
+          data: {
+            height: 50,
+            is_silhouette: true,
+            url: constants.modules.api.auth.facebookTestPicture,
+            width: 50
+          }
+        },
+        id: constants.modules.api.auth.facebookTestId
+      };
+    }
 
-	disconnect() {}
+    throw new Error('Invalid code');
+  }
+
+  disconnect() {}
 
 }
 
