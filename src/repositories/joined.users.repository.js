adb76435c3d0fdcf1cfcf619df5c1b4b14c895fe
jotprabh1 <prabhjot.narula@gmail.com>
2019-10-18 09:50:16 -0300

    fix: getForChallenge doesn't exist (#132)

--- src/repositories/joined.users.repository.js
+++ src/repositories/joined.users.repository.js
@@ -1,4 +1,5 @@
 const {model} = require('../db/models/joined.users.model');
+const UserModel = require('../db/models/user.model');
 const BasePostgresRepository = require('./abstracts/base-postgres.repository');
 
 class JoinedUsersRepository extends BasePostgresRepository {
@@ -24,6 +25,17 @@ class JoinedUsersRepository extends BasePostgresRepository {
     return await this.model.count({
       where: {userId, challengeId}
     }) !== 0;
+  }
+
+  async getForChallenge(challengeId) {
+    return await this.model.findAll({
+      where: {
+        challengeId
+      },
+      include: [{
+        model: UserModel
+      }]
+    });
   }
 
 }
